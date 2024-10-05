533769a7522047d34966762d5d4e40cf0c5f907d
hai2605 <nguyenhaibr5@gmail.com>
2024-10-05 17:44:52 +0700

    Fix BlogBlock element

--- blog-app/src/components/BlogBlock.jsx
+++ blog-app/src/components/BlogBlock.jsx
@@ -4,30 +4,44 @@ import './BlogBlock.css';
 
 function BlogBlock(){
     return(
-        <>
-        <div className='post_block'>
+      <>
+      <div className="post_block">
+        <div className='post_block-item'>
+          
           <a  className='post_block-link' href="https://usth-coders-club.github.io/discord-music-bot-3/" target="_blank">
             <img src={postImage} className="post_block-image" alt="post_image" />
           </a>
+
           <div className="post_block-content">
+
             <h2 className='post_block-content_title'>
               <a  className='post_block-link' href="https://usth-coders-club.github.io/discord-music-bot-3/" target="_blank">
                 [CODE MUSIC BOT FOR DISCORD] - Phần 3: Lập trình các tính năng
               </a>
             </h2>
+
             <p className='post_block-content_description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus ad nihil a, 
               harum adipisci blanditiis aliquam aut in neque unde ipsa tempore! Itaque earum, optio voluptate exercitationem tenetur laborum sunt.
               aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+
           </div>
+
           <div className='post_block-footer'>
+
             <img src={user_ava} alt="user_ava" className="post_block-author_ava" />
             <div className='post_block-info'>
+
               <span className='post_block-author_name'>Đỗ Nhật Thành</span>
               <br />
               <span className='post_block-date'>06 May 2022</span>
+
             </div>
+
           </div>
+
         </div>
+
+      </div>
       </>
     )
 }
