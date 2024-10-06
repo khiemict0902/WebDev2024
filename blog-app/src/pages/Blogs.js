315a7679d0257b99b907f5c6c89a1ea732f6f25f
KasumiYae <kiriha0205@gmail.com>
2024-10-06 11:08:50 +0700

    add blog and resize it in blog

--- blog-app/src/pages/Blogs.js
+++ blog-app/src/pages/Blogs.js
@@ -5,12 +5,32 @@ import BlogBlock from '../components/BlogBlock';
 function Blogs(){
     return(
         <div>
-            Blog
-
-            <Iconplus />
+            <div>
+                Blog
+                <Iconplus />
+            </div>
+            
+            <div >
+                <h1>Featured</h1>
+                <hr className="line"></hr>
+                
+                <div className="blog-blocks">
+                    <BlogBlock />
+                    <BlogBlock />
+                    <BlogBlock />
+                </div>
+            </div>
+            <div>
+                <h1>All Posts</h1>
+                <hr className="line"></hr>
+                <div className="blog-blocks">
+                    <BlogBlock />
+                    <BlogBlock />
+                    <BlogBlock />
+                </div>
+            </div>
         </div>
-        
-    )
+    )   
 }
 
-export default Blogs
+export default Blogs;
