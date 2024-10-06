ae20b58d18ac02a9bf7937768270d1217e4d331a
HuyenanhBA12-006 <155504194+HuyenanhBA12-006@users.noreply.github.com>
2024-10-06 18:53:08 +0700

    Base layout of About Us

--- blog-app/src/pages/AboutUs.jsx
+++ blog-app/src/pages/AboutUs.jsx
@@ -7,42 +7,69 @@ import friendship from './assets/friendship.png'
 function AboutUs(){
     return(
         <div className='ABoutUS'>
-            <div><h1 className='heading1'>About Us</h1>
-            <p1 className='Introduction'>The platform signifies a substantial opportunity as a gift for users who aspire to embark on their journey of sharing ideas online in a professional, yet accessible manner. Its visually appealing design and intuitive interface, combined with an expansive interconnected community, facilitate not only the creation of a personal blog; but also support brand development, identity formation, and alignment with various relevant topics. This renders it particularly effective for attracting and engaging with a global audience. However, one might ponder: what are you waiting for? Begin crafting your blog and delve into the blogging landscape today!</p1>
-            
-            {/* <script>
-                function clickHandler(){
+            <div className="aboutus_container">
+                <div className='introduction'>
+                    <div className="introduction_heading">
+                        <h1>About Us</h1>
+                    </div>
                     
-                }
-            </script> */}
-            </div>
-            <button onClick='clickHandler()'>
-                Explore!    
-            </button>
-            <div className='block'>
-                <div className='box'>
-                    <img className='box icon' src={userinterface} alt="User Interface" />
-                    <h2 className='box highlight'>User-friendly</h2>
-                    <div className='text'> 
-                        <p className='overlay'>With a solid foundation, trusted by many users. We bring a product with many eye-catching, easy-to-use features. Users can easily create blogs and change page properties.</p>
+                    <div className="introduction_description">
+                        <p1 >The platform signifies a substantial opportunity as a gift 
+                        for users who aspire to embark on their journey of sharing ideas online in a professional, 
+                        yet accessible manner. Its visually appealing design and intuitive interface, 
+                        combined with an expansive interconnected community, facilitate not only the creation of a 
+                        personal blog; but also support brand development, identity formation, and alignment with various relevant topics. 
+                        This renders it particularly effective for attracting and engaging with a global audience.
+                        However, one might ponder: what are you waiting for? Begin crafting your blog and delve into the blogging landscape today!</p1>
+                    </div>
+
+                    <div className="explore">
+                        <button className='explore_button' onClick='clickHandler()'>
+                            Explore!    
+                        </button>
                     </div>
                 </div>
 
-                <div className='box'>
-                    <img className='box icon' src={association} alt="Association" />
-                    <h2 className='box highlight'> Huge Association</h2>
-                    <div className='text'>
-                        <p className='overlay'>With the participation of many users from countries around the world, different topics. Users can easily connect, search for useful information, and much more.</p>
+                <div className='block'>
+                    <div className='box'>
+                        <div className="box_icon">
+                            <img className='icon' src={userinterface} alt="User Interface" />
+
+                        </div>
+                        <div className="box_highlight">
+                            <h2 className='highlight'>User-friendly</h2>
+                        </div>
+                        <div className='overlay'> 
+                            <p className='text'>With a solid foundation, trusted by many users. We bring a product with many eye-catching, easy-to-use features. Users can easily create blogs and change page properties.</p>
+                        </div>
+                    </div>
+
+                    <div className='box'>
+                        <div className="box_icon">
+                            <img className='icon' src={association} alt="Association" />
+
+                        </div>
+                        <div className="box_highlight">
+                            <h2 className='highlight'> Huge Association</h2>
+                        </div>
+                        <div className='overlay'>
+                            <p className='text'>With the participation of many users from countries around the world, different topics. Users can easily connect, search for useful information, and much more.</p>
+                        </div>
+                    </div>
+
+                    <div className='box'>
+                        <div className="box_icon">
+                            <img className='icon' src={friendship} alt="Friend Ship" />
+                        </div>
+                        <div className="box_highlight">
+                            <h2 className='hightlight'>Open - Community</h2>
+                        </div>
+                        <div className='overlay'>
+                            <p className='text'>Community with many participants, users can easily connect with each other, share useful information, learn from each other</p>
+                        </div>
                     </div>
                 </div>
 
-                <div className='box'>
-                    <img className='box icon' src={friendship} alt="Friend Ship" />
-                    <h2 className='box hightlight'>Open - Community</h2>
-                    <div className='text'>
-                        <p className='overlay'>Community with many participants, users can easily connect with each other, share useful information, learn from each other</p>
-                    </div>
-                </div>
             </div>
 
         </div>
