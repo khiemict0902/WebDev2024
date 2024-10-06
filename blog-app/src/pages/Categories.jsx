e3ab72ec6eaba4c70efc0c204228814ba89947b8
hai2605 <nguyenhaibr5@gmail.com>
2024-10-06 21:04:05 +0700

    modify blogblock, category page, navbar

--- blog-app/src/pages/Categories.jsx
+++ blog-app/src/pages/Categories.jsx
@@ -1,34 +1,43 @@
 import './Categories.css';
 import BlogBlock from '../components/BlogBlock';
+import NavBar from './NavBar';
+import Footer from '../components/Footer';
 
 function Categories(){
     return(
         <>
-            <div>
-                <header>
-
-                </header>
-                <main className='category_container'>
-                    <div className="section_title">
-                        <h2 className='category_title'> Type I </h2>
+            <header>
+                <NavBar />
+            </header>
+            <main className='category_content'>
+                <div className="category_content_container">
+                    <div className='category_title'>
+                        <h2 >Type I</h2>
                     </div>
-                    <div className='post_block-container'>
-                        <BlogBlock/>
-                        <BlogBlock/>
-                        <BlogBlock/>
-                        <BlogBlock/>
-                        <BlogBlock/>
-                        <BlogBlock/>
-                        <BlogBlock/>
-                        <BlogBlock/>
-                        <BlogBlock/>
-                    </div>
-                </main>
-                <footer>
+                    <div className='category_blogblock_container'>
+                        <div className="blogblock_container">
+                            <BlogBlock/>
+                            <BlogBlock/>
+                            <BlogBlock/>
+                            <BlogBlock/>
+                            <BlogBlock/>
+                            <BlogBlock/>
 
-                </footer>
-            </div>
-            <div></div>
+                        </div>
+
+                        <div class='pagination'>
+                        <a className='pagination_direction previous' href='#'>&laquo; Prev</a>
+                            <a className='active' href='#'>1</a>
+                            <a href='#'>2</a>
+                            <a className='pagination_direction next' href='#'>Next &raquo;</a>
+
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <footer>
+                <Footer />
+            </footer>
         </>
     )
 }
---
+++ blog-app/src/pages/NavBar.jsx
@@ -0,0 +1,60 @@
+import './NavBar.css';
+import { Outlet, Link } from "react-router-dom";
+
+
+
+function NavBar() {
+  return (
+    <>
+      <div className='header'>
+          <nav className='navD'>
+            <ul>
+              <Link className='link' to='/'>
+                <div className='logo'>
+                  Logo
+                </div>
+              </Link>
+            </ul>
+            <ul className='ul links navigation_part'>
+              <li className='li'>
+                <Link className='link' to="/blogs">Blogs</Link>
+              </li>
+              <li className='li'>
+                <Link className='link' to="/post">"Post (tam thoi xoa sau)"</Link>
+              </li>
+              <li className='li categories'>
+                <Link className='link'>Categories</Link>
+                <div className="category_list_container">
+                  <ul className="category_list">  
+                    <li className="category_item"><a href="./Categories" className="category_link">Type I</a></li>
+                    <li className="category_item"><a href="./Categories" className="category_link">Type I</a></li>
+                    <li className="category_item"><a href="./Categories" className="category_link">Type I</a></li>
+                  </ul>
+                
+                </div>
+              </li>
+              <li className='li'>
+                <Link className='link' to="/aboutus">About us</Link>
+              </li>
+            </ul>
+          </nav>
+          {/* <Outlet/> */}
+
+      </div>
+
+        <div className="blog_title">
+          <div className="blog_title_container">
+            <h1 className="blog_name">
+              Blog name
+            </h1>
+            <p className="blog_description">
+              Blog description
+            </p>
+          </div>
+        </div>
+    </>
+
+  )
+}
+
+export default NavBar
