7975856c4d625ed854e7c9eab8976afc16ed2129
AnhVan1111 <anhtv.ba12-007@st.usth.edu.vn>
2024-10-10 13:39:45 +0700

    update Post

--- blog-app/src/components/Post.js
+++ blog-app/src/components/Post.js
@@ -2,16 +2,19 @@ import './Post.css';
 import React, { useState, useEffect } from 'react'; // Import useState
 import NavBar from '../pages/NavBar';
 
-function Post( ) {
+function Post() {
     // Declare state to control Hide/Show
     const [isVisible, setIsVisible] = useState(true); // isVisible is true for the content to be visible 
-    const [posts, setPosts] = useState([]);
+    const [posts, setPosts] = useState({summary: []});
     // Function to handle when pressing hide/show button. This function will invert the state of isVisible.
     const toggleVisibility = () => {
         setIsVisible(!isVisible)    
     };
+    // Initialize state to store the index of the currently selected item
+    const [activeIndex, setActiveIndex] = useState(null);
     
     useEffect(() => {
+        // Get data from JSON file
         const fetchPosts = async () => {
             const response = await fetch('/post-data.json');
             const data  = await response.json();
@@ -19,6 +22,45 @@ function Post( ) {
         }
         fetchPosts();
     }, []);
+
+
+    const renderContent = (content) => {
+        // Check if content is an array
+        if (!Array.isArray(content)) {
+            console.error('Content is not an array:', content);
+            return null; 
+        }
+        return (
+            <div className="post_small-text">
+                {content.map((item, index) => {
+                    switch (item.type) {
+                        case 'paragraph':
+                            return (
+                                <p key={index} className="post_line-post">{item.text}</p>
+                            );
+                        case 'commandList':
+                            return (
+                                <ul key={index} className="post_line-post">
+                                    {item.commands.map((command, commandIndex) => (
+                                        <li key={commandIndex}>{command}</li>
+                                    ))}
+                                </ul>
+                            );
+                        case 'image':
+                            return (
+                                <img key={index} src={item.src} alt={item.alt} className="post_picture" />
+                            )
+                        default:
+                            return null;
+                    }
+                })}
+            </div>
+        )
+    }
+    // Check if posts is an array
+    if (!Array.isArray(posts)) {
+        return <div>No posts available</div>;
+    }
 
     return (
         <>
@@ -30,49 +72,49 @@ function Post( ) {
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
                 </head>
         
-                <body className="layout-post">
-                    <div className="site-content">
-                        <div className="container">
+                <body className="post_layout-post">
+                    <div className="post_site-content">
+                        <div className="post_container">
                             {/* Site Title */}
-                            <div className="main-title">
+                            <div className="post_main-title">
                                 <h1 className="text-title">USTH Coders Club Blog</h1>
-                                <p className="lead">Chia sẻ về lập trình</p>
+                                <p className="post_lead">Chia sẻ về lập trình</p>
                             </div>
         
                             {/* Content */}
-                            <div className="main-content">
+                            <div className="post_main-content">
                                 {/* Begin Article */}
                                 {posts.map(post => ( 
                                     <div key={post.id} className="container">
-                                        <div className="row">
+                                        <div className="post_row">
                                             {/* Post Share */}
-                                            <div className="share">
+                                            <div className="post_share">
                                                 <p>Share</p>
                                             </div>
                                             {/* Post */}
-                                            <div className="post">
-                                                <div className="mainheading">
+                                            <div className="post_post">
+                                                <div className="post_mainheading">
                                                     {/* Author Box */}
-                                                    <div className="info">
-                                                        <div className="re-img">
-                                                            <img src={post.author.image} alt="avatar" className="image"/>
+                                                    <div className="post_info">
+                                                        <div className="post_re-img">
+                                                            <img src={post.author.image} alt="avatar" className="post_image"/>
                                                         </div>
-                                                        <div className="text">
+                                                        <div className="post_text">
                                                             <p>{post.author.name}</p>
                                                             <div>{post.author.gen}</div>  
                                                         </div>
                                                     </div>
                                                     {/* Post Title */}
-                                                    <h1 className="text-title">{post.title}</h1>
+                                                    <h1 className="post_text-title">{post.title}</h1>
                                                 </div>
                                                 {/* Post Feature Image */}
-                                                <img src={post.featureImage} alt="code" className="picture"/>
+                                                <img src={post.featureImage} alt="code" className="post_picture"/>
                                                 {/* End Post Feature Image */}
                                                 {/* Post Content */}
-                                                <div className="article-post">
+                                                <div className="post_article-post">
                                                     {/* Summary List */}
-                                                    <div className="list-post">
-                                                        <div className="block1">
+                                                    <div className="post_list-post">
+                                                        <div className="post_block1">
                                                             <h1>Summary</h1>
                                                             {/* button Hide/Show */}
                                                             <button onClick={toggleVisibility}> 
@@ -86,72 +128,29 @@ function Post( ) {
                                                         isVisible is false => div will hide */}
                                                         {/* list-content */}
                                                         {isVisible && (
-                                                            <div id="block2">
+                                                            <div id="post_block2">
                                                                 <ul>
                                                                     {post.summary.map((item,index) => (
-                                                                        <li key={index} className="topc">
-                                                                        <a href="#">{item}</a>
+                                                                        <li key={index} className="post_topc">
+                                                                        <a href={`#section-${index}`} onClick={() => setActiveIndex(index)}>{item.title}</a>
                                                                     </li>
                                                                     ))}
-                                                                    {/* <li className="topc">
-                                                                        <a href="#Cấu trúc hoạt động của Bot">Cấu trúc hoạt động của Bot</a>
-                                                                    </li>
-                                                                    <li className="topc">
-                                                                        <a href="#handlers.js">handlers.js</a>
-                                                                    </li>
-                                                                    <li className="topc">
-                                                                        <a href="#play.js">play.js</a>
-                                                                    </li>
-                                                                    <li className="topc">
-                                                                        <a href="#pause.js">pause.js</a>
-                                                                    </li>
-                                                                    <li className="topc">
-                                                                        <a href="#resume.js">resume.js</a>
-                                                                    </li>
-                                                                    <li className="topc">
-                                                                        <a href="#skip.js">skip.js</a>
-                                                                    </li>
-                                                                    <li className="topc">
-                                                                        <a href="#back.js">back.js</a>
-                                                                    </li>
-                                                                    <li className="topc">
-                                                                        <a href="#stop.js">stop.js</a>
-                                                                    </li>
-                                                                    <li className="topc">
-                                                                        <a href="#nowplaying.js">nowplaying.js</a>
-                                                                    </li>
-                                                                    <li className="topc">
-                                                                        <a href="#queue.js">queue.js</a>
-                                                                    </li>
-                                                                    <li className="topc">
-                                                                        <a href="#Tổng kết">Tổng kết</a>
-                                                                    </li> */}
                                                                 </ul>
                                                             </div>
                                                         )} 
                                                         {/* End list-content */}
                                                     </div>
                                                     {/* End Summary List */}
-                                                    <p>{post.content}</p>
-                            
-                                                    <h2 id="Cấu trúc hoạt động của Bot">Cấu trúc hoạt động của Bot</h2>
-                                                    <p>Con Bot của chúng ta sẽ có những lệnh cơ bản của một Bot chơi nhạc:</p>
-                                                    <ul>
-                                                        <li>
-                                                            <strong>play</strong>: chơi một bài nhạc
-                                                        </li>
-                                                        {/* Add more list items as necessary */}
-                                                    </ul>
-                                                    <h2 id="handlers.js">handlers.js</h2>
-                                                    <h2 id="play.js">play.js</h2>
-                                                    <h2 id="pause.js">pause.js</h2>
-                                                    <h2 id="resume.js">resume.js</h2>
-                                                    <h2 id="skip.js">skip.js</h2>
-                                                    <h2 id="back.js">back.js</h2>
-                                                    <h2 id="stop.js">stop.js</h2>
-                                                    <h2 id="nowplaying.js">nowplaying.js</h2>
-                                                    <h2 id="queue.js">queue.js</h2>
-                                                    <h2 id="Tổng kết">Tổng kết</h2>
+                                                    <p className="post_small_text">{post.content}</p>
+                                                    
+                                                   
+                                                    {post.summary.map((item, index) => (
+                                                        <div key={index} id={`section-${index}`}>
+                                                            <h1>{item.title}</h1>
+                                                            {renderContent(item.content)}
+                                                        </div>
+                                                    ))}
+                                            
                                                 </div>
                             
                                                 <p>
