a61015bd208e8ec8c6de2c3d2000fa5328142c11
HuyenanhBA12-006 <155504194+HuyenanhBA12-006@users.noreply.github.com>
2024-10-17 19:10:30 +0700

    Fix Footer

--- blog-app/src/components/Footer.jsx
+++ blog-app/src/components/Footer.jsx
@@ -1,28 +1,42 @@
 import React from 'react';
 import './Footer.css';
-import usth from '../assets/usth.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faFacebookF} from '@fortawesome/free-brands-svg-icons'
+import {faInstagram} from '@fortawesome/free-brands-svg-icons'
+import {faEnvelope} from '@fortawesome/free-regular-svg-icons'
+import {faLinkedin} from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
     return (
         <>
-            <div className='footer-container'>
-                <div className='logo'>
-                    <img src={usth} alt='USTH' className='usth' />
-                </div>
-
+            <div className='footer_container'>
                 <div className='group'>
-                        <h1>GROUP 10</h1>
-                        <p>-------------------------------------------------</p>
-                        <p>Pham Duc Khiem:          BA12-095</p>
-                        <p>Luyen Pham Ngoc Khanh:   BA12-093</p>
-                        <p>Tang Van Anh:            BA12-007</p>
-                        <p>Tran Ngoc Viet Anh:      BA12-003</p>
-                        <p>Nguyen Dinh Hai:         BA12-068</p>
-                        <p>Ngo Huyen Anh:           BA12-006</p>
+                    <div className='group_heading'><h1>Get in touch</h1></div>
+                        <div className='group_colume'>
+                            <div className='group_name'>
+                                <p>Tran Ngoc Viet Anh</p>
+                                <p>Ngo Huyen Anh</p>
+                                <p>Tang Van Anh</p>
+                            </div>
+                            <div className='group_name'>
+                                <p>Nguyen Dinh Hai</p>
+                                <p>Pham Duc Khiem</p>
+                                <p>Luyen Pham Ngoc Khanh</p>
+                            </div>
+                        </div>
                 </div>
-
-                <div className="group_contact">
-                        
+                <div className='footer_connect'>
+                    <div className='connect'>
+                        <h1 className='connect_heading'>Connect</h1>
+                        <div className='group_icon'>
+                            <div className='icon'>
+                                <div className='icon_list'><FontAwesomeIcon icon={faFacebookF} style={{color: "#fafafa",}} /></div>
+                                <div className='icon_list'><FontAwesomeIcon icon={faInstagram} style={{color: "#ffffff",}} /></div>
+                                <div className='icon_list'><FontAwesomeIcon icon={faEnvelope} style={{color: "#ffffff",}} />  </div>
+                                <div className='icon_list'><FontAwesomeIcon icon={faLinkedin} style={{color: "#ffffff",}} /></div>
+                            </div>
+                        </div>
+                    </div>                    
                 </div>
             
             </div>
