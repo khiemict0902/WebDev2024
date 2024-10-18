6053ea6818860db86a1eb98de6f412bceaca6cd3
AnhVan1111 <anhtv.ba12-007@st.usth.edu.vn>
2024-10-18 17:05:25 +0700

    fix AboutUs

--- blog-app/src/intro/AboutUs.jsx
+++ blog-app/src/intro/AboutUs.jsx
@@ -1,84 +1,58 @@
-import './AboutUs.css';
-import userinterface from '../assets/userinterface.png';
-import association from '../assets/association.png';
-import friendship from '../assets/friendship.png'
-import NavBar from '../components/NavBar';
+import React from 'react';
+import './AboutUs.css'; // Link to the CSS file
+import BlogImg from '../assets/blog.png'
+import Aboutus from '../assets/aboutus.png'
 
-function AboutUs(){
-    return(
-        <>
-        <NavBar/>
-        <div className='ABoutUS'>
-            <div className="aboutus_container">
-                <div className='introduction'>
-                    <div className="introduction_heading">
-                        <h1>About Us</h1>
-                    </div>
-                    
-                    <div className="introduction_description">
-                        <p1 >The platform signifies a substantial opportunity as a gift 
-                        for users who aspire to embark on their journey of sharing ideas online in a professional, 
-                        yet accessible manner. Its visually appealing design and intuitive interface, 
-                        combined with an expansive interconnected community, facilitate not only the creation of a 
-                        personal blog; but also support brand development, identity formation, and alignment with various relevant topics. 
-                        This renders it particularly effective for attracting and engaging with a global audience.
-                        However, one might ponder: what are you waiting for? Begin crafting your blog and delve into the blogging landscape today!</p1>
-                    </div>
+function AboutUs() {
+    return (
+        <div>
+            {/* <header>
+                <a href="#" className="logo">
+                    <img src="" alt="logo" />
+                </a>
+                <ul className="navbar">
+                    <li>
+                        <a href="#home">Home</a>
+                    </li>
+                    <li>
+                        <a href="#about">About Us</a>
+                    </li>
+                    <li>
+                        <a href="#login">Log In</a>
+                    </li>
+                    <li>
+                        <a href="#home">Home</a>
+                    </li>
+                </ul>
+            </header> */}
 
-                    <div className="explore">
-                        <div className="explore_button">
-                            <a href='/blogs'>
-                                Explore!    
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='block'>
-                    <div className='box'>
-                        <div className="box_img">
-                            <img className='img' src={userinterface} alt="User Interface" />
-
-                        </div>
-                        <div className="box_highlight">
-                            <h2 className='highlight'>User-friendly</h2>
-                        </div>
-                        <div className='overlay'> 
-                            <p className='text'>With a solid foundation, trusted by many users. We bring a product with many eye-catching, easy-to-use features. Users can easily create blogs and change page properties.</p>
-                        </div>
+            <div className="box">
+                <section className="home" id="home">
+                    <div className="home_text">
+                        <h1>Đăng nội dung theo cách của bạn</h1>
+                        <p>Dễ dàng tạo blog độc đáo và tuyệt đẹp.</p>
+                        <a href="#" className="btn">Tạo Blog Của Bạn</a>
                     </div>
 
-                    <div className='box'>
-                        <div className="box_img">
-                            <img className='img' src={association} alt="Association" />
-
-                        </div>
-                        <div className="box_highlight">
-                            <h2 className='highlight'> Huge Association</h2>
-                        </div>
-                        <div className='overlay'>
-                            <p className='text'>With the participation of many users from countries around the world, different topics. Users can easily connect, search for useful information, and much more.</p>
-                        </div>
+                    <div className="home_img">
+                        <img src={BlogImg} alt="blog" />
                     </div>
-
-                    <div className='box'>
-                        <div className="box_img">
-                            <img className='img' src={friendship} alt="Friend Ship" />
-                        </div>
-                        <div className="box_highlight">
-                            <h2 className='hightlight'>Open - Community</h2>
-                        </div>
-                        <div className='overlay'>
-                            <p className='text'>Community with many participants, users can easily connect with each other, share useful information, learn from each other</p>
-                        </div>
-                    </div>
-                </div>
-
+                </section>
             </div>
 
+            <section className="about" id="about">
+                <div className="about_img">
+                    <img src={Aboutus} alt="aboutus" />
+                </div>
+                <div className="about_text">
+                    <h2>Our</h2>
+                    <p>With all that we have, the platform is a gift for those who want to start their journey of sharing ideas online in a professional and easy-to-use way.</p>
+                    <p>With an eye-catching, user-friendly interface, a large connected community, ... the platform not only helps you build a personal blog but also helps develop a personal mark, shape your brand, orient the topic to suit many main topics, attract and connect with the global community.</p>
+                    <p>So what are you waiting for, start creating your own blog and explore the world of blogging today!</p>
+                    <a href="#" className="btn">Learn More</a>
+                </div>
+            </section>
         </div>
-        </>
-    )
+    );
 }
-
-export default AboutUs
+export default AboutUs;
